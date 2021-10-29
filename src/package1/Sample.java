ac649ec38a2319ab5ef406128039d8586c250b22
jumit1996 <jjsatapathy111@gmail.com>
2021-10-29 12:50:43 +0530

    3rd commit

--- src/package1/Sample.java
+++ src/package1/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("New Commit");
 		System.out.println("New Commit 1");
+		System.out.println("New Commit 2");
 	}
 
 }
